469bd53641ec6947fe7ff2ddca7959f35b36c74a
Faisal-Mahmud-Anik <bandafaisalmahmud@gmail.com>
2024-03-29 02:02:12 +0600

    navigating jsx types problem

--- src/01-introduction/03-navigating-jsx-types.problem.tsx
+++ src/01-introduction/03-navigating-jsx-types.problem.tsx
@@ -2,11 +2,13 @@ export const Component = () => {
   return (
     <div
       // How do I figure out what type aria-posinset expects?
-      aria-posinset={}
+      // ans: Hovering over the property or element and read what the say
+      aria-posinset={1}
       // How do I figure out what type onChange expects?
-      onChange={}
-
+      // ans: Hovering over the property or element and read what the say
+      onChange={() => {}}
       // How do I get autocomplete with JSX?
+      // ans: Pressing (ctrl + space)
     />
   );
 };
